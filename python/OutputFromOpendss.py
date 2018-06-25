eb95cecc09373dc39e102ce4045053cf769b029b
luluhahaha <lushawang06@gmail.com>
2018-06-25 13:27:31 -0700

    include opendss

---
+++ python/OutputFromOpendss.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 23 14:16:10 2018
+
+@author: user
+"""
+
+import csv
+import matplotlib.pylab as plt
+def getVoltageProfile():
+    voltageList={}
+    with open('ieee13nodeckt_EXP_VOLTAGES.CSV','rb') as csvfileVoltage:
+        csvreaderVoltage=csv.reader(csvfileVoltage)
+        mycsvVoltage=list(csvreaderVoltage)
+        for row in mycsvVoltage:
+            if  row[0]!= str("Bus") and row[0]!=str("RG60") and row[0]!=str("SOURCEBUS") and row[0]!=str("650"):
+                voltageList[row[0]]=float(row[5])
+            if row[0]==str("650"):
+                voltageList["1"] = float(row[5])
+    
+            
+    print voltageList
+    print list(voltageList.values())
+    
+    return voltageList
+    
+def plotVoltageProfile(voltageList):
+    # plot original voltage
+    lists = sorted(voltageList.items())
+    x,y = zip(*lists)
+    plt.ylim(0.99, 1.06)
+    plt.plot(x,y)
+    plt.plot((0,692),(1.05,1.05),'r')
+    plt.show()
+    
---
+++ python/python_opendss.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 20:51:01 2018
+
+@author: lusha
+"""
+
+import OutputFromOpendss
+#import matplotlib.pyplot as pyplot
+import win32com.client
+
+# call OpenDSS
+dssObj = win32com.client.Dispatch("OpenDSSEngine.DSS")
+dssText = dssObj.Text
+dssCircuit = dssObj.ActiveCircuit
+dssSolution = dssCircuit.Solution
+dssElem = dssCircuit.ActiveCktElement
+dssBus = dssCircuit.ActiveBus
+dssText.Command = "compile 'Master.dss'"
+
+# retrieve output of openDSS and plot voltage
+voltageList = OutputFromOpendss.getVoltageProfile()
+OutputFromOpendss.plotVoltageProfile(voltageList)
+
+# chech voltage
+nodeWithIssue = []
+highestV = 0
+nodeWithHighestV = -1
+for i in list(voltageList.keys()):
+    if voltageList.get(i) > highestV:
+        nodeWithHighestV=i
+        highestV = voltageList.get(i)
+print nodeWithHighestV
+
+if highestV > 1.05:
+    # overvoltage issue
+    print "overvoltage in node ",nodeWithHighestV
+    # control part
+    genName = "Generator.test"
+    dssCircuit.Generators.Name = genName.split(".")[1]
+    oldkvar = dssCircuit.Generators.kvar
+    print oldkvar
+    dssCircuit.Generators.kvar = oldkvar - 100
+    dssSolution.Solve()
+    dssText.Command = "Export Voltages"
+    dssText.Command = "Plot Profile Phases=All"
+    print dssCircuit.Generators.kvar
+    voltageList = OutputFromOpendss.getVoltageProfile()
+    OutputFromOpendss.plotVoltageProfile(voltageList)
+
+else:
+    # no voltage issue
+    print "no voltage issue"
+
