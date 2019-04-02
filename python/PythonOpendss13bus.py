a0e037a192f6373e0f198aa2d201c7a6b8af521e
luluhahaha <lushawang06@gmail.com>
2019-04-02 16:53:32 -0700

    update

--- python/PythonOpendss13bus.py
+++ python/PythonOpendss13bus.py
@@ -36,7 +36,7 @@ def cons(variables, networkGraph, oneCluster, genList, SVQ, neighborConstraintIn
 #    Vlist = variables[len(genList):]
     genLen = len(genList)
     conslist = []
-    '''  
+    
     # Q generation limit
     
     for geni in range(0, len(genList)):
@@ -45,7 +45,7 @@ def cons(variables, networkGraph, oneCluster, genList, SVQ, neighborConstraintIn
         conQ['type'] = 'ineq'
         conQ['fun'] = lambda variables: vs[gennode]["QsupplyMax"] - (vs[gennode]["Qsupply"] + variables[geni])    
         conslist.append(conQ)
-    '''
+    
     # voltage constraint for currentNode
     for nodei in range(0, len(oneCluster)):
         currentNode = oneCluster[nodei]
@@ -57,15 +57,15 @@ def cons(variables, networkGraph, oneCluster, genList, SVQ, neighborConstraintIn
         # constraint for voltage and delta_Q
         conPowerFlow = {}
         conPowerFlow['type'] = 'eq'
-        conPowerFlow['fun'] = partial(variables[genLen + nodei] - Vinitial - np.dot(SVQrow, variables[:genLen]), variables)
+        conPowerFlow['fun'] = lambda variables: variables[genLen + nodei] - Vinitial - np.dot(SVQrow, variables[:genLen])
         conslist.append(conPowerFlow)
         
         #upper constraint for Vnodei
         conVU = {}
         conVU['type'] = 'ineq'
-        conVU['fun'] = partial(1.05 - variables[genLen + nodei], variables)
+        conVU['fun'] = lambda variables: 1.05 - variables[genLen + nodei]
         conslist.append(conVU)
-        '''
+        
         #lower constraint for Vnodei
         conVL = {}
         conVL['type'] = 'ineq'
@@ -81,7 +81,7 @@ def cons(variables, networkGraph, oneCluster, genList, SVQ, neighborConstraintIn
             conVNei['type'] = 'eq'
             conVNei['fun'] = lambda variables: variables[genLen + nodei] - vs[neighborNode]["Vmag"]
             conslist.append(conVNei)
-        '''
+        
    # print len(conslist)
     return conslist
 
